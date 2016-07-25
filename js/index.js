4f3c2b65cb3b7cb19ac18e453fab9738a31559a4
PedroVB <pedrovalorbellido@gmail.com>
2016-07-25 16:37:47 +0200

    Últimos Cambios

--- js/index.js
+++ js/index.js
@@ -1,28 +1,28 @@
-  $(document).on('scroll', function (e) {
-   var alpha = $(document).scrollTop() / 550;
-   $('.navbar-default').css('background-color', 'rgba(0,0,0,' + alpha + ')');
- });
+$(document).on('scroll', function (e) {
+  var alpha = $(document).scrollTop() / 550;
+  $('.navbar-default').css('background-color', 'rgba(0,0,0,' + alpha + ')');
+});
 
-  $(document).ready(function(){
+$(document).ready(function(){
 
-    $(".nav").hide(0).delay(300).fadeIn(3000); 
-    $(".photo").addClass("flip-top");
+  $(".nav").hide(0).delay(300).fadeIn(3000); 
+  $(".photo").addClass("flip-top");
 
-    $('.ancla').click(function(){
-      $('.ancla').css("color", "white");
-      $(this).css("color", "#F7F3BC");
-      var link = $(this);
-      var anchor  = link.attr('href');
-      $('html, body').stop().animate({
-        scrollTop: $(anchor).offset().top
-      }, 1000);
-      return false;
-    });
-
-    $('.skillbar').each(function(){
-      $(this).find('.skillbar-bar').animate({
-        width:$(this).attr('data-percent')
-      },6000);
-    });
-    
+  $('.ancla').click(function(){
+     $('.ancla').css("color", "white");
+     $(this).css("color", "#F7F3BC");
+     var link = $(this);
+     var anchor  = link.attr('href');
+     $('html, body').stop().animate({
+       scrollTop: $(anchor).offset().top
+     }, 1000);
+     return false;
   });
+
+  $('.skillbar').each(function(){
+    $(this).find('.skillbar-bar').animate({
+      width:$(this).attr('data-percent')
+    },6000);
+  });
+  
+});
