6fa5de80ccfd7c57e50b687cfd451633e8804a76
PedroVB <pedrovalorbellido@gmail.com>
2016-07-25 16:10:21 +0200

    Sizes, css, etc

--- js/index.js
+++ js/index.js
@@ -4,21 +4,25 @@
  });
 
   $(document).ready(function(){
+
     $(".nav").hide(0).delay(300).fadeIn(3000); 
     $(".photo").addClass("flip-top");
+
     $('.ancla').click(function(){
       $('.ancla').css("color", "white");
       $(this).css("color", "#F7F3BC");
       var link = $(this);
       var anchor  = link.attr('href');
       $('html, body').stop().animate({
-        scrollTop: jQuery(anchor).offset().top
+        scrollTop: $(anchor).offset().top
       }, 1000);
       return false;
     });
-    jQuery('.skillbar').each(function(){
-    jQuery(this).find('.skillbar-bar').animate({
-      width:jQuery(this).attr('data-percent')
-    },6000);
-  });
+
+    $('.skillbar').each(function(){
+      $(this).find('.skillbar-bar').animate({
+        width:$(this).attr('data-percent')
+      },6000);
+    });
+    
   });
